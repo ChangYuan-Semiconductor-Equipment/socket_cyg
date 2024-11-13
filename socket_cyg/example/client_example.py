f2882cd85e859ef39778d2c6cb21aa86b1846cc6
LiuWei <183074632@qq.com>
2024-11-13 17:03:44 +0800

    style: format code

--- socket_cyg/example/client_example.py
+++ socket_cyg/example/client_example.py
@@ -4,13 +4,15 @@ import os
 from socket_cyg.socket_client import SocketClient
 
 
+FILE_PATH = r"D:\python_workspace\equipment_cyg.rar"
+
+
 if __name__ == '__main__':
-    file_path = r"D:\python_workspace\equipment_cyg.rar"
 
     client1 = SocketClient()
     client1.client_open()
     client1.run_receive_thread()
-    with open(file_path, mode="rb+") as f:
+    with open(FILE_PATH, mode="rb+") as f:
         data = f.read()
         dir_path = f"{os.getcwd()}/a"
         client1.client_send(dir_path.encode("UTF-8") + b"-_-" + data + b"@__@")
@@ -18,7 +20,7 @@ if __name__ == '__main__':
     client2 = SocketClient()
     client2.client_open()
     client2.run_receive_thread()
-    with open(file_path, mode="rb+") as f:
+    with open(FILE_PATH, mode="rb+") as f:
         data = f.read()
         dir_path = f"{os.getcwd()}/b"
         client2.client_send(dir_path.encode("UTF-8") + b"-_-" + data + b"@__@")
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     client3 = SocketClient()
     client3.client_open()
     client3.run_receive_thread()
-    with open(file_path, mode="rb+") as f:
+    with open(FILE_PATH, mode="rb+") as f:
         data = f.read()
         dir_path = f"{os.getcwd()}/c"
         client3.client_send(dir_path.encode("UTF-8") + b"-_-" + data + b"@__@")
@@ -34,7 +36,8 @@ if __name__ == '__main__':
     client4 = SocketClient()
     client4.client_open()
     client4.run_receive_thread()
-    with open(file_path, mode="rb+") as f:
+    with open(FILE_PATH, mode="rb+") as f:
         data = f.read()
         dir_path = f"{os.getcwd()}/d"
         client4.client_send(dir_path.encode("UTF-8") + b"-_-" + data + b"@__@")
+        
