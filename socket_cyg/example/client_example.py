0a9ca2ddb85952b0485bfc3b72a6873be6ef0c85
LiuWei <183074632@qq.com>
2025-11-23 16:44:37 +0800

    feature: update example

--- socket_cyg/example/client_example.py
+++ socket_cyg/example/client_example.py
@@ -5,11 +5,8 @@ from socket_cyg.socket_client import SocketClient
 
 
 if __name__ == "__main__":
-    def my_callback(data: bytes):
-        """示例回调函数。"""
-        print(f"回调收到数据: {data.decode('UTF-8')}")
 
-    client = SocketClient("127.0.0.1", 9001, my_callback)
+    client = SocketClient("127.0.0.1", 9001)
     if client.connect():
         try:
             client.send_data(b"ffffff")
